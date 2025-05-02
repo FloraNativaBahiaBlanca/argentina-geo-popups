bc22c36055fdf8254866db8a0dabebe07dbadc63
Max Ferrer <maxi.fg13@gmail.com>
2025-05-02 19:29:56 -0300

    Fix domain

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/argentina-geo-popups">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
